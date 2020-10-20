30deb79014ea7911e94357d9c46c44a1ef261b93
anushka-srivastava22 <67271376+anushka-srivastava22@users.noreply.github.com>
2020-10-20 16:09:47 +0530

    Add files via upload

---
+++ mul.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n1,n2;
+    cout<<"Enter first number ";
+    cin>>n1;
+    cout<<"Enter second number ";
+    cin>>n2;
+    int mul = n1*n2;
+    cout << "Multiplication of numbers " << mul;
+}
